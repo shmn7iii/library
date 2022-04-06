2c5bb763c3389e4640cf5f4f01db7d36781ec699
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-04-07 00:15:07 +0900

    add: install-tapyrus.sh

---
+++ Tapyrus/install-tapyrus.sh
@@ -0,0 +1,20 @@
+# /bin/sh
+
+# tapyrus core version
+TAPYRUS_CORE_VERSION='0.5.0'
+
+# dependency
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y wget curl git vim 
+
+# download
+wget https://github.com/chaintope/tapyrus-core/releases/download/v${TAPYRUS_CORE_VERSION}/tapyrus-core-${TAPYRUS_CORE_VERSION}-x86_64-linux-gnu.tar.gz -O tapyrus-core-${TAPYRUS_CORE_VERSION}.tar.gz
+tar -xzvf tapyrus-core-${TAPYRUS_CORE_VERSION}.tar.gz
+sudo rm -rf tapyrus-core-${TAPYRUS_CORE_VERSION}.tar.gz
+
+# path
+echo 'export PATH="$HOME/tapyrus-core-$TAPYRUS_CORE_VERSION/bin:$PATH"' >> ~/.bashrc
+
+# reload
+source .bashrc
